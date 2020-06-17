2b34a1c826f7e3e81ec370e88e6048edd9c945ab
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-17 04:30:43 -0700

    update counter with context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 import Parent from './Parent';
+import CounterContext from './CounterContext';
 
 function App() {
   return (
+    <CounterContext.Provider value={30}>
     <div className="App">
      <Parent/>
     </div>
+    </CounterContext.Provider>
   );
 }
 
--- src/Child.js
+++ src/Child.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CounterContext from './CounterContext';
 
 const Child=()=>{
+    let CounterValue=useContext(CounterContext);
     return(
         <div>
             <h1>
                 This is a child 
             </h1>
+            <h2>
+                This is counter value {CounterValue}
+            </h2>
         </div>
     );
 }
